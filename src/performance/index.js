7c620948eca32d4eb74e684de057745e2b33457a
wangzhenggui <jianjia.wzg@raycloud.com>
2022-12-13 15:23:46 +0800

    fix: 性能指标使用web-vitals来统计

--- src/performance/index.js
+++ src/performance/index.js
@@ -8,14 +8,35 @@
  * 
  * Copyright (c) 2022 by wangzhenggui jianjia.wzg@raycloud.com, All Rights Reserved. 
  */
-import watchFp from './watchFp'
-import watchTiming from './watchTiming'
-import watchLcp from './watchLcp'
+// import watchFp from './watchFp'
+// import watchTiming from './watchTiming'
+// import watchLcp from './watchLcp'
+import { onCLS, onFCP, onFID, onINP, onLCP, onTTFB } from 'web-vitals'
+import { getPageInfo } from '../utils/util'
+import { lazySendCache } from '../report'
+
+const sendPerformance = ({ id, name, delta, rating }) => {
+    const reportData = {
+        info: {
+            id, name, delta, rating,
+        },
+        subType: name,
+        type: 'performance',
+        pageURL: getPageInfo(),
+    }
+    lazySendCache(reportData)
+}
 
 const performanceHandle = () => {
-    watchTiming()
-    watchFp()
-    watchLcp()
+    onCLS(sendPerformance)
+    onFCP(sendPerformance)
+    onFID(sendPerformance)
+    onINP(sendPerformance)
+    onLCP(sendPerformance)
+    onTTFB(sendPerformance)
+    // watchTiming()
+    // watchFp()
+    // watchLcp()
 }
 
 export default performanceHandle
